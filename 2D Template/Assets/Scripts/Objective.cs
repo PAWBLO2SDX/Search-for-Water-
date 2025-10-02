af1a44708eb2168dfb85a744802841098682f22c
MUKILTEO\1722453 <davewilson260@gmail.com>
2025-10-02 13:35:01 -0700

    made objective text boxes

---
+++ 2D Template/Assets/Scripts/Objective.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+
+public class Objective: MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartObjective()
+    {
+        textComponent.text = string.Empty;
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        // Type each character one by one
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
