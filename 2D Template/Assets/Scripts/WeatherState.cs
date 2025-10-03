92671484daa1e8acbca42972e22ce84507f0c32c
DRAGONBIRDS\drago <camfry77@gmail.com>
2025-10-02 23:10:18 -0700

    Heat-O-Meter is finally done, almost done with the weather system.

---
+++ 2D Template/Assets/Scripts/WeatherState.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class WeatherState : MonoBehaviour
+{
+    private int currentStateIndex;
+    public static State currentWeatherState;
+    public delegate void WeatherChangedHandler(State state);
+    public event WeatherChangedHandler OnWeatherChanged;
+
+    public enum State
+    {
+        SandStorm,
+        Sunny,
+        HeatWave,
+        Cloudy,
+        Windy,
+    }
+
+    public State[] WeatherStateOrder = new State[]
+    {
+        State.Cloudy,
+        State.Sunny,
+        State.SandStorm,
+        State.Cloudy,
+        State.SandStorm,
+        State.HeatWave,
+        State.Sunny,
+        State.Windy,
+        State.SandStorm,
+        State.Sunny,
+    };
+
+    void Start()
+    {
+        currentStateIndex = 0;
+        currentWeatherState = WeatherStateOrder[currentStateIndex];
+    }
+
+    public State GetCurrentWeatherState()
+    {
+        return WeatherStateOrder[currentStateIndex];
+    }
+
+    public void CycleWeatherState()
+    {
+        currentStateIndex = (currentStateIndex + 1) % WeatherStateOrder.Length;
+        currentWeatherState = WeatherStateOrder[currentStateIndex];
+        print("The weather is now: " + WeatherStateOrder[currentStateIndex]);
+
+        OnWeatherChanged?.Invoke(currentWeatherState);
+    }
+    public int GetNextWeatherStateIndex()
+    {
+        return (currentStateIndex + 1) % WeatherStateOrder.Length;
+    }
+}
