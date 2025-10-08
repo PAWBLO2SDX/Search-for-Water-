f2ca29cbdfbe8af30d4232bd468f89e25221434b
MUKILTEO\1725632 <1725632@mukilteo.wednet.edu>
2025-10-08 13:32:56 -0700

    Inventory slots and item alomost done

--- 2D Template/Assets/InvetnoryItem.cs
+++ 2D Template/Assets/InvetnoryItem.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
-public class InvetnoryItem : MonoBehaviour
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Transform parentAfterDrag;
+
+    [Header("UI")]
+    public Image icon;
+
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("Begin drag");
+        icon.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
+        icon.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
+        transform.localPosition = Vector3.zero;
     }
 }
